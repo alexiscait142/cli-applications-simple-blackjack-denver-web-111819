6c5793bc222bee9d6726602b6bfdc0ab0b837296
Alexis Chilinski <chilinski.a@gmail.com>
2019-11-11 20:20:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,9 +30,9 @@ end
 
 def initial_round
   # code #initial_round here
-  total = 0
-  total += deal_card
-  total += deal_card
+  deal_card
+  deal_card
+  total = deal_card + deal_card
   display_card_total(total)
 end
 
